6f13561707c370775342b5a98759d1c3d80c310e
marikf98 <marikf98@gmail.com>
2024-01-21 14:38:19 +0200

    finished heuristic class

--- pancake/heuristics.py
+++ pancake/heuristics.py
@@ -1,8 +1,30 @@
 
-
 def base_heuristic(_pancake_state):
-    return 0
+# we assume that the state is in this string format - 5,4,3,2,1.
+    heuristic = 0
+    pancake_stack = _pancake_state.get_state_str().split(',')
+    pancake_stack = [int(num) for num in pancake_stack]
+    for i in range(0, len(pancake_stack)):
+        if pancake_stack[i] != len(pancake_stack) - i:
+            heuristic += pancake_stack[i]
+
+    return heuristic
+
 
 def advanced_heuristic(_pancake_state):
-    return 0
+    # like the heuristic that in the slides of the pratical session
 
+    heuristic = 0
+    pancake_stack = _pancake_state.get_state_str().split(',')
+    pancake_stack = [int(num) for num in pancake_stack]
+
+    if pancake_stack[0] != len(pancake_stack):
+        heuristic += 1
+
+    for i in range(0, len(pancake_stack)):
+        if i == len(pancake_stack) - 1:
+            break
+        if abs(pancake_stack[i] - pancake_stack[i + 1]) > 1:
+            heuristic += 1
+
+    return heuristic
--- pancake/main.py
+++ pancake/main.py
@@ -1,3 +1,4 @@
+from pancake import heuristics
 from search import search
 from pancake_state import pancake_state
 from heuristics import *
@@ -5,7 +6,10 @@ from heuristics import *
 if __name__ == '__main__':
     goal_state = "4,3,2,1"
     pancake_input = "4,2,3,1"
+    #pancake_input = "5,3,6,4,1,2"
     pancake_state = pancake_state(pancake_input)
+    print(heuristics.base_heuristic(pancake_state))
+    print(heuristics.advanced_heuristic(pancake_state))
     search_result = search(pancake_state, base_heuristic, goal_state)
 
 
